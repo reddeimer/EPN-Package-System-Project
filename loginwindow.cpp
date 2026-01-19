05e422d40b889b9becc0ecd51df71765653c06ff
reddeimer <da8086895@gmail.com>
2026-01-19 18:46:25 -0500

    feat:fix login

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -1,6 +1,9 @@
 #include "loginwindow.h"
 #include "ui_loginwindow.h"
+#include <fstream>
+#include <string>
 // #include <QDebug>
+using namespace std;
 
 loginwindow::loginwindow(UserType type, QWidget *parent)
     : QWidget(parent)
@@ -21,6 +24,32 @@ loginwindow::~loginwindow()
     delete ui;
 }
 
+bool loginwindow::validateLogin(const QString &cedula, const QString &password) {
+    fstream file(dataFile.toStdString(), ios::in);
+
+    if (!file.is_open()) {
+        ui->messageLabel->setText("No se pudo abrir el archivo");
+        return false;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        size_t pos = line.find(';');
+        if (pos == string::npos) {
+            continue;
+        }
+        string fileCedula = line.substr(0, pos);
+        string filePassword = line.substr(pos+1);
+
+        if (fileCedula == cedula.toStdString() && filePassword == password.toStdString()) {
+            file.close();
+            return true;
+        }
+    }
+    file.close();
+    return false;
+}
+
 void loginwindow::on_loginButton_clicked() {
     // qDebug() << "LOGIN PRESIONADO";
     ui ->messageLabel->setText("Probando...");
@@ -33,11 +62,17 @@ void loginwindow::on_loginButton_clicked() {
     }
 
     // Prueba
-    if (cedula == "123" && password == "123") {
-        ui ->messageLabel->setText("Login correcto");
+    // if (cedula == "123" && password == "123") {
+    //     ui ->messageLabel->setText("Login correcto");
+    // } else {
+    //     ui->messageLabel->setText("Usuario incorrecto");
+    // }
+    if (validateLogin(cedula, password)) {
+        ui->messageLabel->setText("Login correcto");
     } else {
         ui->messageLabel->setText("Usuario incorrecto");
     }
+
 }
 
 void loginwindow::on_registerButton_clicked() {
